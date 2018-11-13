e05f75f950b5cf7a04ca69eeb3e2e79125cd0dae
Yuri Dogandjiev <yudogan@microsoft.com>
2018-11-13 14:42:00 -0800

    Add test app logic

---
+++ index.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var csp = require('helmet-csp');
+var serveStatic = require('serve-static');
+
+var app = express();
+app.use(csp({
+  directives: {
+    blockAllMixedContent: true,
+    frameSrc: [
+      "https:",
+      "sip:",
+      "tel:"
+    ]
+  }
+}));
+app.use(serveStatic(__dirname + '/public'));
+
+var port = process.env.port || 3000;
+app.listen(port, function () {
+  console.log('Listening on http://localhost:' + port);
+});
